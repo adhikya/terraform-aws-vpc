9f20be03866d473c28289424903f53d88b7521d2
adhikya <ch.adhikya@gmail.com>
2025-06-29 12:04:38 +0800

    tesitng

--- data.tf
+++ data.tf
@@ -7,6 +7,14 @@ data "aws_vpc" "default" {
 }
 
 
+data "aws_route_table" "main" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+        name = "association.main"
+        values = ["true"]
+    }
+}
+
 /*output "azs_info" {
     value = data.aws_availability_zones.available
 } */
